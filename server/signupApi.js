7ff3f1fa42cbf86ef1c987d42fe74f72da86bcee
Nguyen Xuan Vinh <nguyenxuanvinh55th2@gmail.com>
2016-04-28 09:10:37 +0700

    again

--- server/signupApi.js
+++ server/signupApi.js
@@ -1,4 +1,5 @@
 import {Parties} from '../imports/api/parties/index.js';
+//Restivus to api khoi tao
 var Api = new Restivus({
     useDefaultAuth: true,
     prettyJson: true
@@ -7,14 +8,12 @@ var Api = new Restivus({
   var Cryptr = require('cryptr'),
       cryptr = new Cryptr('ntuquiz123');
 
-
-
 Api.addRoute('signup/:info', {authRequired: false}, {
     get: function () {
       var decryptedString = cryptr.decrypt(this.urlParams.info);
-      console.log(decryptedString);
-      
+    //  console.log(decryptedString);
       var party = JSON.parse(decryptedString);
+
       Parties.insert(party);
         return {
           statusCode: 200,
--- server/startup.js
+++ server/startup.js
@@ -7,15 +7,23 @@ import {Email} from 'meteor/email';
 var Cryptr = require('cryptr'),
     cryptr = new Cryptr('ntuquiz123');
 
-
+//ma hoa thong tin nguoi dung
 var encryptedString = cryptr.encrypt('{"name":"nguyen van a","email":"abc@gmail.com", "password":"123123123"}'),
-    decryptedString = cryptr.decrypt(encryptedString);
+    decryptedString = cryptr.decrypt(encryptedString);//giai ma thong tin
 console.log(encryptedString);
+console.log(decryptedString);
+//? asmostphere tai khoan nguoi dung va cong mac dinh
 process.env.MAIL_URL = 'smtp://nguyenxuanvinhict@gmail.com:7955132350@smtp.gmail.com:465/';
 
-// Email.send({
-//   to: 'duong@s2corp.vn',
-//   from: 'nguyenxuanvinhict@gmail.com',
-//   subject: 'test Email',
-//   text: encryptedString
-// });
+//chuyen huong den template
+SSR.compileTemplate('emailText',Assets.getText("hello.html"));
+//chuyen html
+var html = SSR.render("emailText",{text:encryptedString});
+console.log(html);
+//cach gui email
+Email.send({
+  to: 'nguyenxuanvinh55th2@gmail.com',
+  from: 'nguyenxuanvinhict@gmail.com',
+  subject: 'test Email template with link',
+  html:html
+});
